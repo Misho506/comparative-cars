67cc6a91ec600db316a501edc3f9a484bb170b4f
Mauricio Lopez <mlopez@pernixlabs.com>
2019-11-02 11:36:20 -0600

    Add constants of carList, colors for style and model of car

---
+++ src/app/models/car.ts
@@ -0,0 +1,8 @@
+export interface Car {
+  img: string;
+  model: string;
+  year: number;
+  brand: string;
+  price: number;
+  details: string;
+}
---
+++ src/constant/cars.js
@@ -0,0 +1,144 @@
+// import { agya } from "../assets/images/index";
+
+export default [
+  // SUZUKI
+  {
+    img: "vitara.png",
+    model: "Vitara",
+    year: 2019,
+    brand: "Suzuki",
+    price: 23490,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "s-cross.png",
+    model: "S-cross",
+    year: 2019,
+    brand: "Suzuki",
+    price: 23490,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "ignis.png",
+    model: "Ignis",
+    year: 2019,
+    brand: "Suzuki",
+    price: 17990,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "jimny.png",
+    model: "Jimny",
+    year: 2019,
+    brand: "Suzuki",
+    price: 23990,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "celerio.png",
+    model: "Celerio",
+    year: 2019,
+    brand: "Suzuki",
+    price: 16990,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  // TOYOTA
+  {
+    img: "agya.png",
+    model: "Agya",
+    year: 2020,
+    brand: "Toyota",
+    price: 15900,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "corolla.png",
+    model: "Corolla",
+    year: 2020,
+    brand: "Toyota",
+    price: 26900,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "prado.png",
+    model: "Land Cruiser Prado",
+    year: 2020,
+    brand: "Toyota",
+    price: 64400,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "wagon.png",
+    model: "land cruiser station wagon",
+    year: 2020,
+    brand: "Toyota",
+    price: 150400,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  // HYUNDAI
+  {
+    img: "ioniq.png",
+    model: "Ioniq eléctrico",
+    year: 2020,
+    brand: "Hyundai",
+    price: 38900,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "creta.png",
+    model: "creta",
+    year: 2020,
+    brand: "Hyundai",
+    price: 25900,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "santaFe.png",
+    model: "santa Fe",
+    year: 2020,
+    brand: "Hyundai",
+    price: 42900,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "elantra.png",
+    model: "elantra",
+    year: 2020,
+    brand: "Hyundai",
+    price: 28900,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  // HONDA
+  {
+    img: "cr-v.png",
+    model: "cr-v",
+    year: 2020,
+    brand: "Honda",
+    price: 38950,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "city.png",
+    model: "city",
+    year: 2020,
+    brand: "Honda",
+    price: 23500,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "civic-sedan.png",
+    model: "civic sedan",
+    year: 2020,
+    brand: "Honda",
+    price: 30950,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+  {
+    img: "civic-type-r.png",
+    model: "civic type r",
+    year: 2020,
+    brand: "Honda",
+    price: 49950,
+    details: "Es el mejor auto del mercado. Es económico, seguro y con mucho espacio dentro del automóvil, lo que lo convierte en un automóvil cómodo."
+  },
+];
